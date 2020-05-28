4db8e101d5135f052c18ed891164bb2b1f2439c5
Rishi Malhotra <rishirmalhotra@gmail.com>
2020-05-28 00:40:15 -0400

    Create python_practice.py

---
+++ app/python_practice.py
@@ -0,0 +1,25 @@
+x = 1
+print(x) #>1
+
+#breakpoint() for temporary debugging purposes
+
+x = 10
+print(x) 
+
+y = 5
+
+print(y)
+
+y = y + 100
+print(y)
+
+y += 100 # equiv shorthand for y= Y +100
+print(y)
+print (1,2,3) # can print many values separated by commas
+
+z = 12
+
+if z == 37:
+    print("YES")
+else:
+    print("NO")
